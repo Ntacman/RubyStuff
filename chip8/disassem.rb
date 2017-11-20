ee567d9b022e86cde03610711382b522bb348671
Ntacman <Ntacman@users.noreply.github.com>
2017-11-20 00:23:39 -0600

    more changes

--- chip8/disassem.rb
+++ chip8/disassem.rb
@@ -15,26 +15,31 @@ end
 def translate(op)
   case
   when op == '00e0'
-    puts "#{op} - clear screen"
     "#{op} - clear screen"
   when op == '00ee'
-    puts "#{op} - return from subroutine"
     "#{op} - return from subroutine"
   when op == '0000'
-    puts "unknown/bad OP"
-    "unknown/bad OP"
+    "unknown/bad OP #{op}"
   when op[0] == '0' && check_last_byte(op, 0) # Check the last byte to make sure it isn't 0
-    puts "#{op} - jump to routine at #{op[1..-1]}"
     "#{op} - jump to routine at #{op[1..-1]}"
-  when op[0] == 1
-    puts "#{op} - jump to address #{op[1..-1]}"
+  when op[0] == '1'
     "#{op} - jump to address #{op[1..-1]}"
-  when op[0] == 2
-    puts "#{op} - call subroutine at #{op[1..-1]}"
+  when op[0] == '2'
     "#{op} - call subroutine at #{op[1..-1]}"
+  when op[0] == '3'
+    "#{op} - skip next instruction if register V#{op[1]} == #{op[2..-1]}"
+  when op[0] == '4'
+    "#{op} - skip next instruction if register V#{op[1]} != #{op[2..-1]}"
+  when op[0] == '5'
+    "#{op} - skip next instruction if register V#{op[1]} == Register V#{op[2]}"
+  when op[0] == '6'
+    "#{op} - set V#{op[1]} to #{op[2..-1]}"
+  when op[0] == '7'
+    "#{op} - set register V#{op[1]} to V#{op[1]} + #{op[2..-1]}"
+  when op[0] == '8' && op[3] == '0'
+    "#{op} - Set the value of V#{op[1]} to value of V#{op[2]}(V#{op[1]} == V#{op[2]})"
   else
-    puts "unknown/bad OP"
-    "unknown/bad OP"
+    "unknown/bad OP #{op}"
   end
 end
 
@@ -54,5 +59,9 @@ end
 ops = read_rom(rom)
 
 ops.each do |op|
-  File.open("#{rom}.txt", 'a+') { |file| file.write( "\n#{translate(op)}") }
+  if op == ops.first
+    File.open("#{rom}.txt", 'a+') { |file| file.write( "#{translate(op)}") }
+  else
+    File.open("#{rom}.txt", 'a+') { |file| file.write( "\n#{translate(op)}") }
+  end
 end
