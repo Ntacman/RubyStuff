27b380dd63379e9dda66e2ec28970fb1876a30a5
Ntacman <Ntacman@users.noreply.github.com>
2017-11-19 19:55:22 -0600

    more stuff

--- chip8/disassem.rb
+++ chip8/disassem.rb
@@ -15,12 +15,32 @@ end
 def translate(op)
   case
   when op == '00e0'
-    puts "#{op} - Clear Screen"
-    "#{op} - Clear Screen"
+    puts "#{op} - clear screen"
+    "#{op} - clear screen"
+  when op == '00ee'
+    puts "#{op} - return from subroutine"
+    "#{op} - return from subroutine"
+  when op == '0000'
+    puts "unknown/bad OP"
+    "unknown/bad OP"
+  when op[0] == '0' && check_last_byte(op, 0) # Check the last byte to make sure it isn't 0
+    puts "#{op} - jump to routine at #{op[1..-1]}"
+    "#{op} - jump to routine at #{op[1..-1]}"
+  when op[0] == 1
+    puts "#{op} - jump to address #{op[1..-1]}"
+    "#{op} - jump to address #{op[1..-1]}"
+  when op[0] == 2
+    puts "#{op} - call subroutine at #{op[1..-1]}"
+    "#{op} - call subroutine at #{op[1..-1]}"
   else
-    puts "Unknown/Bad OP"
-    "Unknown/Bad OP"
+    puts "unknown/bad OP"
+    "unknown/bad OP"
   end
+end
+
+# Used when checking the 0x0nnn opcode to make sure the opcode isn't actually 0x000
+def check_last_byte(op, value)
+  return true if op[2..-1].to_i > value
 end
 
 def letter?(lookAhead)
@@ -32,7 +52,6 @@ def numeric?(lookAhead)
 end
 
 ops = read_rom(rom)
-ops.each { |op| translate(op) }
 
 ops.each do |op|
   File.open("#{rom}.txt", 'a+') { |file| file.write( "\n#{translate(op)}") }
